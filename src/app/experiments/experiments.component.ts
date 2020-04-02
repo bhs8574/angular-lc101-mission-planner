1d446003ee3972964bd85531f08aace4558f7ef7
bhs8574 <bhs8574@gmail.com>
2020-04-02 15:38:14 -0500

    finished bonus missions.

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = ["Mars soil sample", "Plant growth in habitat", "Human bone density"];
+  experimentBeingEdited: string = null;
+  
   constructor() { }
+
+  add(newExperiment: string) {
+    this.experiments.push(newExperiment);
+    
+  }
+
+  remove(oldExperiment: string) {
+    let index = this.experiments.indexOf(oldExperiment);
+    this.experiments.splice(index, 1);
+  }
+
+  edit(changedExperiment: string) {
+    this.experimentBeingEdited = changedExperiment;
+  }
+
+  save(changedExperiment: string, oldExperiment: string) {
+    let index: number = this.experiments.indexOf(oldExperiment);
+    this.experiments[index] = changedExperiment;
+    this.experimentBeingEdited = null;
+  }
 
   ngOnInit() {
   }
