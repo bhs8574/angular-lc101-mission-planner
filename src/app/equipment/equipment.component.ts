381e8a073001d79e98ce05e37e3d66602d40d308
bhs8574 <bhs8574@gmail.com>
2020-04-02 15:19:46 -0500

    Added All bonus features to the equipment section.  Duplicates are allowed because it's reasonable to want to bring multiple copies of an item.

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class EquipmentComponent implements OnInit {
 
   equipment: string[] = ["Habitat dome", "Drones", "Food containers", "Oxygen tanks"];
+  equipmentBeingEdited: string = null;
 
   constructor() { }
+
+  add(equipmentPiece: string) {
+    this.equipment.push(equipmentPiece);
+    
+  }
+
+  remove(equipmentPiece: string) {
+    let index = this.equipment.indexOf(equipmentPiece);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(equipmentPiece: string) {
+    this.equipmentBeingEdited = equipmentPiece;
+  }
+
+  save(equipmentPiece: string, oldEquipmentPiece: string) {
+    let index: number = this.equipment.indexOf(oldEquipmentPiece);
+    this.equipment[index] = equipmentPiece;
+    this.equipmentBeingEdited = null;
+  }
 
   ngOnInit() {
   }
